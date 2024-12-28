2d699dbadfb1b057f7c016f5c6b4661e1cd0eaa9
amitx13 <amitkvs981@gmail.com>
2024-12-28 05:53:32 +0530

    building for production

--- Admin/src/main.tsx
+++ Admin/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,11 +5,9 @@ import { RecoilRoot } from 'recoil'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <RecoilRoot>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </RecoilRoot>
-  </StrictMode>,
+    </RecoilRoot>,
 )
